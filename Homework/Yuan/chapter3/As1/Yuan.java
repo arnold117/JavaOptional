43909740680d2bc1e872179d4c082e20e8be8591
arnold117 <60877940+arnold117@users.noreply.github.com>
2020-10-03 18:12:29 +0800

    Create Yuan.java

---
+++ Homework/Yuan/chapter3/As1/Yuan.java
@@ -0,0 +1,21 @@
+/**
+ * Yuan
+ */
+public class Yuan {
+
+    public static void main(String[] args) {
+        int count = 0;
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int j2 = 0; j2 < 10; j2++) {
+                    if (i > j && j > j2) {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        System.out.println("一共有" + count + "个分机号码");
+    }
+}
