f46ae138fa769bc6d050ed20195fd058be2a75ca
arnold117 <60877940+arnold117@users.noreply.github.com>
2020-10-03 16:52:08 +0800

    Create Test1.java

---
+++ Test/Test1.java
@@ -0,0 +1,17 @@
+public class Test1 {
+
+  public static void main(String a[]) {
+
+    System.out.print("*");
+
+    System.out.println();
+
+    System.out.println("**");
+
+    System.out.println();
+
+    System.out.println("***");
+
+  }
+
+}
