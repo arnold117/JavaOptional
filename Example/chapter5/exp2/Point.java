b8fee0d99a879d814cdcb680ff6f607a20256092
arnold117 <60877940+arnold117@users.noreply.github.com>
2020-10-11 00:00:43 +0800

    Create Point.java

---
+++ Example/chapter5/exp2/Point.java
@@ -0,0 +1,34 @@
+package classeg.chapter5.exp2;
+
+public class Point {
+    int x, y;
+
+    {
+        // 初始化块
+    }
+
+    public Point(int x, int y) {
+        // 有参数的构造方法
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point() {
+        // 无参数构造方法
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Point p1 = new Point();
+        Point p2 = new Point(1,2);
+        System.out.println(p1);
+        System.out.println(p2);
+    }
+}
