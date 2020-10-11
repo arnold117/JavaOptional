0578437e69055d6de7ca43946d6289fccc5406c2
arnold117 <60877940+arnold117@users.noreply.github.com>
2020-10-11 18:32:48 +0800

    update

---
+++ Test/chapter1/Test1.java
@@ -0,0 +1,19 @@
+package test.chapter1;
+
+public class Test1 {
+
+  public static void main(String a[]) {
+
+    System.out.print("*");
+
+    System.out.println();
+
+    System.out.println("**");
+
+    System.out.println();
+
+    System.out.println("***");
+
+  }
+
+}
