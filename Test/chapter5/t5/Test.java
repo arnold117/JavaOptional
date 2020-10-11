7270ee497efa4303b05e4c5ebc2897b93a094ad5
arnold117 <60877940+arnold117@users.noreply.github.com>
2020-10-11 19:01:50 +0800

    Create Test.java

---
+++ Test/chapter5/t5/Test.java
@@ -0,0 +1,17 @@
+package test.chapter5.t5;
+
+public class Test {
+    static int count;
+
+    public static void main(String args[]) {
+
+        int sum=0;
+
+        for (int count=0;count<4;count++)
+
+            sum+=count;
+
+        System.out.println(count);
+
+    }
+}
