e6d029eccc186c62cc2c0abe762164b9d77b921b
arnold117 <60877940+arnold117@users.noreply.github.com>
2020-10-11 19:01:48 +0800

    Create Test.java

---
+++ Test/chapter5/t4/Test.java
@@ -0,0 +1,13 @@
+package test.chapter5.t4;
+
+public class Test {
+    static int age;
+
+    public static void main (String args []) {
+
+        age = age + 1;
+
+        System.out.println("age=" + age);
+
+    }
+}
