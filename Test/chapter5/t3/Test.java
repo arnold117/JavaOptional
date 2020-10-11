a51e05805539e4ccb7ac3035a5eda14ddb3ccd2c
arnold117 <60877940+arnold117@users.noreply.github.com>
2020-10-11 19:01:44 +0800

    Create Test.java

---
+++ Test/chapter5/t3/Test.java
@@ -0,0 +1,11 @@
+package test.chapter5.t3;
+
+public class Test {
+//    long a[ ] = new long[10];
+//
+//    public static void main( String arg[ ] ){
+//
+//        System.out.println(a[6]);
+//
+//    }
+}
