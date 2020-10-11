2629dd29a26daca55672408c2fb0b73b30ef50c7
arnold117 <60877940+arnold117@users.noreply.github.com>
2020-10-11 19:01:38 +0800

    Create My.java

---
+++ Test/chapter5/t10/My.java
@@ -0,0 +1,19 @@
+package test.chapter5.t10;
+
+public class My {
+    int value;
+
+    public static void main(String args[]) {
+
+        My  x = new My();
+
+        if (x==null)
+
+            System.out.print("No Object");
+
+        else
+
+            System.out.print(x.value);
+
+    }
+}
